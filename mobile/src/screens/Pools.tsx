c784b28dc5f5ace8f5e054888abba78a95fef242
Gabriel <gabrielsideprojects@gmail.com>
2023-01-11 17:20:28 -0300

    Add Pools List

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,43 @@
-import { VStack, Icon } from "native-base";
+import { useEffect, useState , useCallback} from 'react'
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons"
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect} from '@react-navigation/native'
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { api } from '../services/api'
+import { PoolCard, PoolPros } from '../components/PoolCard'
+import { Loading } from '../components/Loading'
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
+ 
 export function Pools() {
-
+    const [isLoading, setIsLoading] = useState(false)
+    const [pools, setPools] = useState<PoolPros[]>([]);
+    const Toast = useToast()
     const { navigate } = useNavigation();
+
+    async function fetchPools(){
+        try {
+         setIsLoading(true)
+         const response =  await api.get('/pools')
+         setPools(response.data.pools)
+         console.log(response.data.pools)
+        } catch (error) {
+            console.log('error', error)
+
+            Toast.show({
+                title:'Não foi possível carregar os bolões',
+                placement:'top',
+                bgColor: 'red.500'
+               })
+        } finally { 
+            setIsLoading(false)
+        }
+    }
+
+    useFocusEffect(useCallback(()=>{
+        fetchPools();
+    },[]))
 
     return (
         <VStack flex={1} bgColor={"gray.900"}>
@@ -18,6 +49,19 @@ export function Pools() {
                     onPress={()=> navigate('find')}
                     />
             </VStack>
+            {
+                isLoading ? <Loading/> : 
+                <FlatList
+                    data={pools}
+                    ListEmptyComponent={()=> <EmptyPoolList/>}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item })=> <PoolCard data={item}/>}
+                    px={5}
+                    _contentContainerStyle={{ pb: 10}}
+                    showsVerticalScrollIndicator={false}
+                />
+            }
+           
         </VStack>
     );
 }
